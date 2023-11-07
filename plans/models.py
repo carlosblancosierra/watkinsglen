eb4d3ee05a8d514af504bdb6337103f9ddfbc6ea
Carlos Blanco <carlosblancosierra@gmail.com>
2023-11-07 15:52:15 +0100

    Feat: gallery update

--- plans/models.py
+++ plans/models.py
@@ -6,9 +6,10 @@ class Plan(models.Model):
     name = models.CharField(max_length=100)
     floorplan = models.FileField(upload_to='floorplans/', null=True, blank=True)
     rendering = models.ImageField(upload_to='renderings/', null=True, blank=True)
-    rooms = models.PositiveIntegerField()
-    bathrooms = models.PositiveIntegerField()
-    half_baths = models.PositiveIntegerField()
+    sqft = models.PositiveIntegerField(null=True, blank=True)
+    rooms = models.PositiveIntegerField(null=True, blank=True)
+    bathrooms = models.PositiveIntegerField(null=True, blank=True)
+    half_baths = models.PositiveIntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.name
