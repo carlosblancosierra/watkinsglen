409592c472481d5714d1a9644d02e0ce5adee687
Carlos Blanco <carlosblancosierra@gmail.com>
2024-05-15 22:10:24 +0200

    [Fix] Homwsite migration, images inline

--- image/admin.py
+++ image/admin.py
@@ -11,14 +11,12 @@ class ImageTagAdmin(admin.ModelAdmin):
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('image', 'display_tags')
-
     def display_tags(self, obj):
         return ", ".join([tag.tag for tag in obj.tags.all()])
 
 @admin.register(Gallery)
 class GalleryAdmin(admin.ModelAdmin):
     list_display = ('name',)
-    inlines = [GalleryImageInline]
 
 @admin.register(GalleryImage)
 class GalleryImageAdmin(admin.ModelAdmin):
